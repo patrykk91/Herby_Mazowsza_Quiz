6d49876f94d1f6af0a1ccf9c5c18809bd6a7f38a
Patryk Kamiński <patrykkaminski@outlook.com>
2019-09-23 21:35:57 +0200

    Create user interface

---
+++ app/src/main/java/pl/curlycode/herbymazowszaquiz/MainActivityFragment.java
@@ -0,0 +1,23 @@
+package pl.curlycode.herbymazowszaquiz;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
