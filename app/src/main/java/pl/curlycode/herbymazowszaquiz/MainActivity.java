e15dbccef88d32b8fcd3c0e4f9a9ef7c34e0ca77
Patryk Kamiński <patrykkaminski@outlook.com>
2019-09-24 18:20:00 +0200

    Implement logic for preferences activity in Main class

--- app/src/main/java/pl/curlycode/herbymazowszaquiz/MainActivity.java
+++ app/src/main/java/pl/curlycode/herbymazowszaquiz/MainActivity.java
@@ -1,13 +1,27 @@
 package pl.curlycode.herbymazowszaquiz;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CHOICES  = "pref_number_of_choices";
+    public static final String REGIONS = "pref_RegionsToInclude";
+
+    private boolean phoneDevice = true;
+
+    private boolean preferencesChange = true;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        PreferenceManager.setDefaultValues(this, R.xml.preferences,false);
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(preferenceChangeListener);
     }
 
     @Override
@@ -39,4 +55,37 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+        private SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+                preferencesChange = true;
+
+                MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+                if (key.equals(CHOICES)) {
+                    quizFragment.updateGuessRows(sharedPreferences);
+
+                    quizFragment.resetQuiz();
+
+                } else if (key.equals(REGIONS)) {
+
+                    Set<String> regions = sharedPreferences.getStringSet(REGIONS, null);
+
+                    if (regions != null && regions.size() > 0) {
+                        quizFragment.updateRegions(sharedPreferences);
+                        quizFragment.resetQuiz();
+                    } else {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        regions.add(getString(R.string.default_region));
+                        editor.putStringSet(REGIONS,null);
+                        editor.apply();
+
+                        Toast.makeText(MainActivity.this, R.string.default_region_message, Toast.LENGTH_SHORT).show();
+                    }
+                    Toast.makeText(MainActivity.this,R.string.restarting_quiz, Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
 }
